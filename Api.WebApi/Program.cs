a0804f38314500203c2b0254c1a364e0cbcffa66
AlexR11-26 <n00355997@upn.pe>
2026-01-16 17:43:42 -0500

    b

--- Api.WebApi/Program.cs
+++ Api.WebApi/Program.cs
@@ -11,44 +11,45 @@ builder.Services.AddSingleton(new DbConfig(connectionString));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API PRUEBA", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API PRUEBA",
+        Version = "v1"
+    });
 });
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
-        policy =>
-            policy
-                .AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod()
+    options.AddPolicy("AllowAll", policy =>
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod()
     );
 });
 
 builder.Services.AddScoped<ClienteDAO>();
 builder.Services.AddScoped<ClienteHandler>();
-
 builder.Services.AddScoped<DetalleOrdenDAO>();
 builder.Services.AddScoped<DetalleOrdenHandler>();
-
 builder.Services.AddScoped<CategoriaDAO>();
 builder.Services.AddScoped<CategoriaHandler>();
-
 builder.Services.AddScoped<ProductoDAO>();
 builder.Services.AddScoped<ProductoHandler>();
-
 builder.Services.AddScoped<OrdenDAO>();
 builder.Services.AddScoped<OrdenHandler>();
 
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API PRUEBA v1");
+});
+
 app.UseCors("AllowAll");
 
 app.MapControllers();
